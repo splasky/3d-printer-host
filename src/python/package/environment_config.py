6e6f1a311ea102a8dc993df2318e7d8feab1bb11
Splasky <henry860326@yahoo.com.tw>
2017-04-21 09:49:28 +0800

    change data type to redis

--- src/python/package/environment_config.py
+++ src/python/package/environment_config.py
@@ -1,69 +1,73 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 # vim:fenc=utf-8
-# Last modified: 2017-04-18 15:49:54
+# Last modified: 2017-04-20 20:43:35
 
 import json
 from package.debug import PrintException
 import getpass
 import getpass
-DEFAULTPATH = "/tmp/3dprinter_config"
+import os
 
 
 class config_data(object):
 
     def __init__(self):
-        self.DataBase_IP = ""
-        self.DataBase_User = ""
-        self.DataBase_Password = ""
-        self.DataBase_Name = ""
-        self.DataBase_Name = ""
-        self.DataBase_Port = 0
+        self.redis_ip = ""
+        self.redis_master = ""
+        self.redis_slaver = ""
+        self.redis_port = 0
 
 
 class UseConfigs(object):
 
-    def __init__(self):
+    def __init__(self, path):
         self.config = dict()
+        self.config_file_path = path
+
+    def check_config(self):
+        return os.path.isfile(self.config_file_path)
 
     def make_input(self):
         config = dict()
-        config["DataBase_IP"] = raw_input("data base ip:")
-        config["DataBase_Name"] = getpass.getpass("data base name:")
-        config["DataBase_User"] = raw_input("data base login name:")
-        config["DataBase_Password"] = getpass.getpass("data base password:")
-        config["DataBase_Port"] = int(raw_input("data base port:"))
+        config["redis_ip"] = raw_input("redis ip:")
+        config["redis_name"] = getpass.getpass("redis master:")
+        config["redis_slaver"] = raw_input("redis slaver:")
+        config["redis_port"] = int(raw_input("redis port:"))
         return config
 
-    def make_config(self, path=DEFAULTPATH):
+    def make_config(self):
         try:
             self.config = self.make_input()
-            with open(path, 'w') as f:
+            with open(self.config_file_path, 'w') as f:
                 f.write(json.dumps(
                     self.config, separators=(',', ':'), sort_keys=True,
                 ).encode('utf-8'))
+            return True
         except:
             PrintException()
+            return False
 
-    def load_config(self, path=DEFAULTPATH):
+    def load_config(self):
         try:
-            if path is "":
-                raise RuntimeError("path is not correct!")
-
-            with open(path, "r") as f:
+            with open(self.config_file_path, "r") as f:
                 data = f.read()
                 config = json.loads(data)
                 self.config = dict((key, value) for key, value in config.items())
 
+            return True
         except:
             PrintException()
+            return False
 
     def print_config(self):
         print(self.config)
 
-    def modify_config(self, path=DEFAULTPATH):
+    def modify_config(self):
         try:
-            self.make_config(path)
+            self.make_config(self.config_file_path)
             print("Change config file success.")
+            return True
         except:
             PrintException()
+            return False
