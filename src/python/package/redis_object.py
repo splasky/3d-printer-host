051adf2221ffb1280f4f25458deb2ebacb1d1a0e
Splasky <henry860326@yahoo.com.tw>
2017-04-21 11:51:04 +0800

    finsh redis_object and test on python 2.7 success.

--- src/python/package/redis_object.py
+++ src/python/package/redis_object.py
@@ -1,13 +1,14 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 # vim:fenc=utf-8
-# Last modified: 2017-04-21 09:45:06
+# Last modified: 2017-04-21 11:48:52
 
 
 # master:'control'
 # slaver:'sensor'
 
 import redis
+import time
 
 
 class redis_handler(object):
@@ -16,15 +17,30 @@ class redis_handler(object):
         self.redis_handler = redis.StrictRedis(host=Host, port=Port, db=0)
         self.master = master
         self.slaver = slaver
-        self.pubsub = self.redis_handler.pubsub()
+        self.pubsub = self.redis_handler.pubsub(ignore_subscribe_messages=True)
         self.pubsub.subscribe(self.master)
+        self.__isStopped = False
 
     def send(self, data):
         self.redis_handler.publish(self.slaver, data)
         return True
 
     def recv(self):
-        message = self.pubsub.get_message()
-        return message
+        while True:
+            message = self.pubsub.get_message()
+            if message:
+                return message
+            time.sleep(0.001)
 
     def listen(self):
+        while not self.__isStopped:
+            message = self.pubsub.get_message()
+            if message:
+                yield message
+            time.sleep(0.001)
+
+    def close_listen(self):
+        self.__isStopped = True
+
+    def __del__(self):
+        self.__isStopped = False
