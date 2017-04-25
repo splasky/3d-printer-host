10a119f6616ea539dfb524e861416a5318fc7986
Splasky <henry860326@yahoo.com.tw>
2017-04-25 20:24:23 +0800

    Remove listen() function in redis_object
    redis.py have default listen function

--- src/python/package/redis_object.py
+++ src/python/package/redis_object.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 # vim:fenc=utf-8
-# Last modified: 2017-04-21 11:48:52
+# Last modified: 2017-04-25 20:24:14
 
 
 # master:'control'
@@ -19,7 +19,6 @@ class redis_handler(object):
         self.slaver = slaver
         self.pubsub = self.redis_handler.pubsub(ignore_subscribe_messages=True)
         self.pubsub.subscribe(self.master)
-        self.__isStopped = False
 
     def send(self, data):
         self.redis_handler.publish(self.slaver, data)
@@ -31,16 +30,6 @@ class redis_handler(object):
             if message:
                 return message
             time.sleep(0.001)
-
-    def listen(self):
-        while not self.__isStopped:
-            message = self.pubsub.get_message()
-            if message:
-                yield message
-            time.sleep(0.001)
-
-    def close_listen(self):
-        self.__isStopped = True
 
     def __del__(self):
         self.__isStopped = False
